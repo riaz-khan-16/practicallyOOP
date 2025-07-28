9dd5d9e860d05c77fd2a8de4039648eaa77c6242
riaz-khan-16 <goriazmte@gmail.com>
2025-07-28 14:35:21 +0600

    class vs object

---
+++ 2.class_vs_object.py
@@ -0,0 +1,12 @@
+##this is class
+class calculator:
+     def add(self, a,b):
+            return a+b
+#this is object
+calc=calculator()
+
+#use this obecjct to call a method
+calc.add(5,6)
+print(calc.add(5,6))
+
+
